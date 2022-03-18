17fb2ae2f33b5fcf97ea78726a5ad3cfad154dfa
4507982 <84102186+4507982@users.noreply.github.com>
2022-03-18 13:56:18 -0500

    Add files via upload

---
+++ folder_b/backupFiles.py
@@ -0,0 +1,9 @@
+import os
+import shutil
+source = input("enter sorce folder name: ")
+destenation = input("enter destenation folder name: ")
+source = source+"/"
+destenation = destenation+"/"
+listOfFiles = os.listdir(source)
+for file in listOfFiles:
+    shutil.copy((source+file),destenation)
